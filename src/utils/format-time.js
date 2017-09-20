402d91a8d0d1e0356246647f28529384a9387840
Eva Raymond <evathecoderr@outlook.com>
2017-09-20 22:23:34 +0100

    util functions

---
+++ src/utils/format-time.js
@@ -0,0 +1,15 @@
+// handleTimestamp: function(unix) {
+//     return moment.unix(unix).format('dddd');
+//   },
+  
+//   handleHours: function(unix) {
+//     return moment.unix(unix).format('LT');
+//   },
+  
+
+//USE IP BASED GEOLOCATOR TO DISPLAY WEATHER AT START-UP
+//UPDATE LOCAL TIME-> SHOW TIME BASED ON TIMEZONE OF CITY, LAT&LON
+//IF CAN'T GET WEATHER AT START-UP, RENDER A HOW-TO-USE INFO INSTEAD/DEMO WEATHER SAMPLE
+
+//color: idx%2 ? '#B3DBFF' : '#663000',
+// backgroundColor: idx%2 ? '#0074D9' : '#FF851B',
---
+++ src/utils/index.js
@@ -0,0 +1,4 @@
+export * from './animate-icons';
+export * from './climacons-svg';
+export * from './format-time';
+export * from './weather-api';
